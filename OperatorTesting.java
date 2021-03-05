f7fad03d3f6ce4742c2d5423fed2afdb601e6f6a
CarlosHernandez4045 <carloshernandez4045@gmail.com>
2021-03-05 08:08:18 -0700

    5th commit

--- OperatorTesting.java
+++ OperatorTesting.java
@@ -4,17 +4,17 @@ public class OperatorTesting {
         int b = 10;
 
         while(b < 10){
-            System.out.println("The indicated variable is less than 10");
+            System.out.println("The indicated integer is less than 10");
             return;
         }
         
         while(b > 10){
-            System.out.println("The indicated variable is more than 10");
+            System.out.println("The indicated integer is more than 10");
             return;
         }
 
         while(b == 10){
-            System.out.println("The indicated variable is equal to 10");
+            System.out.println("The indicated integer is equal to 10");
             return;
         }
     }
