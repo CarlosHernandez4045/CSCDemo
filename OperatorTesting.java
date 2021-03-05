2b715c634e8b0676b49f7c6640ceac82a805935a
CarlosHernandez4045 <carloshernandez4045@gmail.com>
2021-03-05 08:06:42 -0700

    4th commit

---
+++ OperatorTesting.java
@@ -0,0 +1,21 @@
+public class OperatorTesting {
+    
+    public static void main(String[] args) {
+        int b = 10;
+
+        while(b < 10){
+            System.out.println("The indicated variable is less than 10");
+            return;
+        }
+        
+        while(b > 10){
+            System.out.println("The indicated variable is more than 10");
+            return;
+        }
+
+        while(b == 10){
+            System.out.println("The indicated variable is equal to 10");
+            return;
+        }
+    }
+}
